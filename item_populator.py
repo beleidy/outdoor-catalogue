fe701fda6645c7deaabd30316d6020ab13548263
Amr Elbeleidy <amr@elbeleidy.me>
2019-02-15 09:11:18 +0000

    adjust item_populator to connect to local port after kubectl proxy

--- item_populator.py
+++ item_populator.py
@@ -1,8 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, User, Category, Item
 
-engine = create_engine('sqlite:///itemcatalogue.db')
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+engine = create_engine(
+    f"postgresql://postgres:{DB_PASSWORD}@127.0.0.1:5432/outdoor-catalogue")
 
 Base.metadata.bind = engine
 
@@ -23,38 +26,33 @@ cycling_array = [
     ("Hybrid Bike", "A bike that you can use anywhere"),
     ("Panniers", "Bags that hang off your bike"),
     ("Tool Kit", "A set of tools to fix your bike when things go wrong"),
-    ]
+]
 camping_array = [
     ("Tent", "Your home away from home"),
     ("Sleeping Bag", "Slide into this bag, and sleep warmly"),
-    ("Insulation Mat",
-     "A mat to go under your sleeping bag"
+    ("Insulation Mat", "A mat to go under your sleeping bag"
      " to insulate you from the cold ground"),
     ("Torch", "Let there be light!"),
-    ]
+]
 hiking_array = [
     ("Hard Shell", "Protects you from water and wind"),
     ("Soft Shell", "A warm jacket that's neither waterproof, nor windproof"),
     ("Baselayer", "The layer touching your skin"),
-    ("Boots",
-     "Insert your feet into these for a more comfortable hike"),
+    ("Boots", "Insert your feet into these for a more comfortable hike"),
     ("Walking Poles",
      "Walking sticks, but not just for the elderly and infirm"),
-    ("Insulating Jacket",
-     "Down down down... this jacket will keep you warm, "
+    ("Insulating Jacket", "Down down down... this jacket will keep you warm, "
      "but totally non-breathable"),
-    ]
+]
 running_array = [
     ("Running Shoes",
      "Shoes that make your run easier on your feet and knees"),
     ("Socks", "Pieces of cloth that sit between your feet and shoes"),
-    ("Running Shorts",
-     "No, these shorts don't run away from you, "
+    ("Running Shorts", "No, these shorts don't run away from you, "
      "but will make you run more with more comfort"),
-    ("Running Top",
-     "Because there is only so much tanning you can do "
+    ("Running Top", "Because there is only so much tanning you can do "
      "before the burning kicks in"),
-    ]
+]
 
 for item in cycling_array:
     i = Item(name=item[0], description=item[1], category_id=1)
